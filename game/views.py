88b308e25d3a1bf6fb985d1d6808d8bc4f2c8476
Alexis Santos <Alexis.Santos@icloud.com>
2016-05-20 12:06:27 -0400

    Access Stackshare's tool explore API end point, print names of first three tools in the console

--- game/views.py
+++ game/views.py
@@ -10,12 +10,19 @@ from django.conf import settings
 import requests
 
 root_url = "https://api.stackshare.io/"
+access_token_string = "&access_token=" + settings.STACKSHARE_API_KEY
 
 def start(request):
 
     template = "start.html"
 
-    print settings.STACKSHARE_API_KEY
+    # Get 50 random tools from Stackshare
+    stackshare = requests.get(root_url + "v1/tools/explore?layer_id=1" + access_token_string)
+
+    # Print the names of the first three tools Stackshare returns
+    print stackshare.json()[1]['name']
+    print stackshare.json()[2]['name']
+    print stackshare.json()[3]['name']
 
     return render_to_response(template, dict({
     }), context_instance=RequestContext(request))
