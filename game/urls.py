b9ba56cdd2dade2f9d58a7b9f39727b26767920b
Alexis Santos <Alexis.Santos@icloud.com>
2016-05-20 11:32:54 -0400

    Added game app
    No need to add it to settings.py since it won't use the/a database

---
+++ game/urls.py
@@ -0,0 +1,4 @@
+from django.conf.urls import *
+
+urlpatterns = patterns('game.views',
+)
---
+++ game/views.py
@@ -0,0 +1,17 @@
+import json
+
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response, get_object_or_404
+from django.core.urlresolvers import reverse
+from django.template import RequestContext
+from django.template.loader import render_to_string
+from django.conf import settings
+
+def start(request):
+
+    template = "start.html"
+
+    print settings.STACKSHARE_API_KEY
+
+    return render_to_response(template, dict({
+    }), context_instance=RequestContext(request))
