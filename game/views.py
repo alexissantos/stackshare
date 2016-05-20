be537429034f38548cfea03ad0de104e30a3152c
Alexis Santos <Alexis.Santos@icloud.com>
2016-05-20 12:29:24 -0400

    Start page now displays the names and images of four random tools from the Stackshare API

--- game/views.py
+++ game/views.py
@@ -19,10 +19,20 @@ def start(request):
     # Get 50 random tools from Stackshare
     stackshare = requests.get(root_url + "v1/tools/explore?layer_id=1" + access_token_string)
 
-    # Print the names of the first three tools Stackshare returns
-    print stackshare.json()[1]['name']
-    print stackshare.json()[2]['name']
-    print stackshare.json()[3]['name']
+    # The following print lines are for debugging in the console
+    # Print the names of the first tool Stackshare returns
+    print stackshare.json()[0]['name']
+
+    # Print the resons people like it
+    for reason in stackshare.json()[0]['reasons']:
+        print reason['one_liner']
+
+    # Print a space
+    print '\n'
 
     return render_to_response(template, dict({
+        "tool_one": stackshare.json()[0],
+        "tool_two": stackshare.json()[1],
+        "tool_three": stackshare.json()[2],
+        "tool_four": stackshare.json()[3],
     }), context_instance=RequestContext(request))
